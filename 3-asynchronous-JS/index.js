0b2b092a52d1f3bbb9e69fec0465e658ae6e90b5
Mareike Heuer <mareike.heuer@hotmail.com>
2024-03-22 00:11:52 +0100

    Section 5.2 (41) The Problem with Callbacks: Callback Hell

---
+++ 3-asynchronous-JS/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+// We will use this dogtile
+// Read the dog breed
+// Do an http request to get a random image of a dog with this breed
+// Save this image to another textfile
+
+// By the end, we will see the problem using all the callback functions
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+
+  // To get the data, we have to call the end method
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      if (err) return console.log(err.message);
+      console.log(res.body.message);
+
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        console.log('Random dog image saved to file');
+      });
+    });
+});
