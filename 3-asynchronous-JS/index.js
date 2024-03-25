71b691a23202910eaeef9108a5f52eacd1bf1b58
Mareike Heuer <mareike.heuer@hotmail.com>
2024-03-25 14:37:52 +0100

    Section 5.6 (45) Returning Values from Async Functions

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -24,7 +24,7 @@ const writeFilePro = (file, data) => {
   });
 };
 
-// Async means, this is special function that keeps runningin the background, while rest of the code runs in the event loop
+// Async means, this is special function that keeps running in the background, while rest of the code runs in the event loop
 // Async also automatically returns a promise
 const getDogPic = async () => {
   try {
@@ -40,11 +40,99 @@ const getDogPic = async () => {
     console.log('Random dog image saved to file!');
   } catch (err) {
     console.log(err);
+    throw err;
   }
+  return '2: READY';
 };
 
-getDogPic();
+// FINAL CODE
+// Declare a function inside paranthesis and call it right away without declaring a named function which has to be called later on
+(async () => {
+  try {
+    console.log('1: Will get dog pics');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pics');
+  } catch (err) {
+    console.log('ERROR');
+  }
+})();
 
+/////// UNDERSTANDING HOW ASYNC WORKS ///////
+
+///////EXAMPLE ///////////
+/* 
+console.log('1: Will get dog pics');
+getDogPic();
+console.log('2: Done getting dog pics');
+
+Output 
+1: Will get dog pics
+2: Done getting dog pics
+Breed: corgi
+https://images.dog.ceo/breeds/corgi-cardigan/n02113186_5841.jpg
+Random dog image saved to file!
+
+Async function ran in the background
+When JS engine sees the getDogPic() function, it gets sent to the background and rest of the code continues running
+When the data is all resolved, it will start printing all the logs. 
+
+*/
+
+/////// EXAMPLE 2 /////////
+
+/*
+console.log('1: Will get dog pics');
+const x = getDogPic();
+console.log(x);
+console.log('2: Done getting dog pics');
+
+/* 
+Output
+
+1: Will get dog pics
+Promise { <pending> }
+2: Done getting dog pics
+Breed: corgi
+https://images.dog.ceo/breeds/corgi-cardigan/n02113186_4536.jpg
+Random dog image saved to file!
+
+
+Why is x variable Promise {pending} ?
+An async function returns a promise automatically. 
+Instead of logging '2: READY', it tells us that its a Promise which is still pending. 
+JS cannot know that x will be the READY string at some point. So it simply moves on. 
+At the time that JS knows that x should be the string, the code has long finished executing.
+But what if we really wanted return that string and log it to the console?
+Then we would have to treat the async function as a proimise, use the then or async/await method on it. 
+*/
+
+/////////// EXAMPLE 3 //////////
+/*
+console.log('1: Will get dog pics');
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log('2: Done getting dog pics');
+  })
+  .catch((err) => {
+    console.log('ERROR');
+  });
+
+/* 
+Output
+1: Will get dog pics
+Breed: corgi
+https://images.dog.ceo/breeds/corgi-cardigan/n02113186_10972.jpg
+Random dog image saved to file!
+2: READY
+2: Done getting dog pics
+
+- getDogPic returns a promise
+- We can use .then medhod to get acces to its value
+
+
+//////// Previous example
 /*
 readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
