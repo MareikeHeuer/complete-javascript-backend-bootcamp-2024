90260477a89d9c67d93c670743dccb8d5845af07
Mareike Heuer <mareike.heuer@hotmail.com>
2024-03-25 12:53:26 +0100

    Section 5.4 (43) Building Promises

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -6,22 +6,45 @@ const superagent = require('superagent');
 // Do an http request to get a random image of a dog with this breed
 // Save this image to another textfile
 
-// By the end, we will see the problem using all the callback functions
-
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  // To get the data, we have to call the end method
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        console.log('Random dog image saved to file');
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('Could not find that file!');
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file!');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random image saved to file!');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+/* 
+  - readFilePro returns a Promise, on that we can use the .then method, 
+  - then the callback function in it will also return a promise
+  - We can then chain the next then handler on that
+  - The result variable will be the resolved value of the Promise returned by the previous handler
+  */
