d7d711bf3f28c12ac7936f75bea661382feadf48
Mareike Heuer <mareike.heuer@hotmail.com>
2024-03-26 12:34:49 +0100

    Section 6.6 (52) Starting Our API: Handling GET Requests

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,19 +1,26 @@
+const fs = require('fs');
 const express = require('express');
 
 // Create variable called app (standard) and assign the result of calling express
 // This will add a bunch of methods to our app variable
 const app = express();
 
-// Define route
-// call app with request method and pass url as argument and callback function
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// Read data before route handler since callack function in route handler will run in event loop and shouldn't have blocking code there
+// JSON.parse converts json to array of js objects
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint');
+// Define route
+// Call app with request method and pass url as argument and callback function
+// ALWAYS specify API version
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    // Data is envelope for our data. We specify it and the data will then intern have an object which contains the response we want to send
+    data: { tours },
+  });
 });
 
 // Start up server
