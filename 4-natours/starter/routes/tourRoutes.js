4aed5113e207fe3b9765890277e2d044fe7574b4
Mareike Heuer <mareike.heuer@hotmail.com>
2024-03-29 05:43:24 +0100

    Section 6.19 (65) Chaining Multiple Middleware Functions

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -3,13 +3,14 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
+// Handle data check middleware outside of route handlers, so they are only concerned with CRUD
 router.param('id', tourController.checkID);
 
 // Once a request is made to one of these routes, the request will enter the middleware stack, and when the route matches, request will happen
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
