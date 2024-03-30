d6a40aeb8a031b2e42e3eb0e7d58efe895ca08ee
Mareike Heuer <mareike.heuer@hotmail.com>
2024-03-30 13:46:46 +0100

    Section 8.4 (85) Creating a Simple Tour Model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -21,6 +21,27 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
+// mongoose.Schema used to specify a schema for our data
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    // This will ensure the name is unique
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// Convention to ALWAYS use capital letter for model and variable names
+const Tour = mongoose.model('Tour', tourSchema);
+
 // This will get us the global environment in which the code runs
 console.log(app.get('env'));
 // Node variables
