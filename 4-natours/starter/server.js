82ff5bdb0d11737a374c058f4e15d002f3552769
Mareike Heuer <mareike.heuer@hotmail.com>
2024-03-30 13:17:27 +0100

    Section 8.2 (83) Connecting Our Database with the Express App

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+// To connect to local use mongose.connect(process.env.DATABASE_LOCAL)
+
+// This one here below is for connecting to host
+mongoose
+  .connect(DB, {
+    // Options for dealing with deprecation warnings
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFineAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 // This will get us the global environment in which the code runs
 console.log(app.get('env'));
