3b3c3dabb4a19574a638261de46a86ae202361f0
Mareike Heuer <mareike.heuer@hotmail.com>
2024-04-01 20:23:41 +0200

    Section 8.19 (100) Making the API Better: Aliasing

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -7,6 +7,13 @@ const router = express.Router();
 // router.param('id', tourController.checkID);
 
 // Once a request is made to one of these routes, the request will enter the middleware stack, and when the route matches, request will happen
+
+// 127.0.0.1:3000/api/v1/tours?limit=5&sort=-ratingsAverage,price
+// Run middleware function which will manipulate incoming query object before executing getAllTours handler
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
 router
   .route('/')
   .get(tourController.getAllTours)
