4747bd9ea51b431ec206f7786bf843082be59340
Mareike Heuer <mareike.heuer@hotmail.com>
2024-04-03 03:38:11 +0200

    Section 8.26 (107) Aggregation Middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -117,8 +117,15 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  console.log(docs);
   // docs is all documents returned by search query
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  // this points to the current aggregation object
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
   next();
 });
 
