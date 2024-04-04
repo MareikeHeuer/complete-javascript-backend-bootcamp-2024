a4e1b842e3caf55556925dd20c2c0a4cfe1f91ed
Mareike Heuer <mareike.heuer@hotmail.com>
2024-04-04 11:41:01 +0200

    Section 9.3 (112) Handling Unhandled Routes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -16,11 +16,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -28,5 +23,14 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+/* Since middleware is added to the stack in the order it is defined in the code,
+this middleware will only run if the previous two routes are not matched */
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
