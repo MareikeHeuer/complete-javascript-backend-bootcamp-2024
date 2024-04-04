1ad7146928741a99068abc5de6856f5b8662dd59
Mareike Heuer <mareike.heuer@hotmail.com>
2024-04-04 12:06:48 +0200

    Section 9.5 (114) Implementing a Global Error Handling Middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -27,9 +27,26 @@ app.use('/api/v1/users', userRouter);
 /* Since middleware is added to the stack in the order it is defined in the code,
 this middleware will only run if the previous two routes are not matched */
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+  // We create an error and define its status and status code properties on it, so that
+  // our error handling middleware can use it in the next step
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  /* If next receives an argument, Express will always assume there is an error
+   and will skip all other middlewares in the middleware stack,
+   and send the error we passed in to the global error handling middleware */
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  // For errors not created by us but somewhere else like the node app for eg
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
