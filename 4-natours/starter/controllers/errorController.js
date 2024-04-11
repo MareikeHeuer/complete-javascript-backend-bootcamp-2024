eaf1dd99b517974ea04a53f698121705b456d1f3
Mareike Heuer <mareike.heuer@hotmail.com>
2024-04-11 16:23:12 +0200

    Section 9.9 (118) Errors during Development vs Production

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,11 +1,40 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown error: don't leak error details to client
+  } else {
+    // 1. Log error
+    console.error('ERROR 🎇', err);
+    // 2. Send generic message
+    res.status(500).json({
+      statis: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   console.log(err.stack); // shows us where the error happened
   // For errors not created by us but somewhere else like the node app for eg
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
